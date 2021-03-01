563375d35f9c724078ddb372b0a39776dcec37e1
lazzyspider <adityapandya7001@gmail.com>
2021-03-01 17:32:45 +0530

    8/10 tasks done

--- README.md
+++ README.md
@@ -3,14 +3,14 @@
 - [x] Higher id boxes will have a higher z-index.
 - [x] Users can add multiple boxes.
 - [x] To select a box, click on it. Highlight the selected box.
-- [ ] Use W-A-S-D or arrow keys on the keyboard to move the selected box.
-- [ ] Use the ‘delete’ key on the keyboard to remove the selected box.
-- [ ] A button to toggle keyboard control.(*no listener should be open when this button status
+- [x] Use W-A-S-D or arrow keys on the keyboard to move the selected box.
+- [x] Use the ‘delete’ key on the keyboard to remove the selected box.
+- [x] A button to toggle keyboard control.(*no listener should be open when this button status
 is off).
 - [ ] Please provide a ReadMe file with your codebase, detailing on how to build and deploy
 your code in the local browser.
 
 ### Additional tasks
-- [ ] Create a hardcoded custom rectangular fence and ensure all the boxes stay within the
+- [x] Create a hardcoded custom rectangular fence and ensure all the boxes stay within the
 fence during movement.
 - [ ]  Optimise the UI and modularize your code.
--- src/app/generator-box-container/generator-box-container.component.ts
+++ src/app/generator-box-container/generator-box-container.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-generator-box-container',
@@ -10,47 +10,116 @@ export class GeneratorBoxContainerComponent implements OnInit {
   @ViewChild('newTestBox', { static: false }) testBox: ElementRef;
   private selectedBoxId: string = null;
   private idCount: number = -1;
+  public keyEvents : boolean = true;
+  x: number=0;
+  y:number=0;
+  width: number=0;
+  height: number=0;
 
   constructor(private renderer: Renderer2) { }
   ngOnInit() { }
 
   ngAfterViewInit() {
+   const{x,y,width,height} = this.testBox.nativeElement.getBoundingClientRect();
+   this.x = x-2;
+   this.y = y+1;
+   this.width = width-2;
+   this.height = height-2;
+   console.log(x+' - '+y+' w ='+width+' h='+height);
+              
+
+
+
     
       window.addEventListener('keydown', (event: KeyboardEvent) => {
+
+        if(!this.keyEvents){
+          event.preventDefault();
+          return;
+        }
+
         if(this.selectedBoxId){
           const keycode = event.key;
           switch(keycode){
             case 'w' : {
-
-              const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
+              
+              // const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
+              
+              
               const v = document.getElementById(this.selectedBoxId);
-              v.style.top = y-1+'px';
+              const {x,y} = v.getBoundingClientRect();
+              console.log(this.x+' - '+this.y);
+              console.log(x+ '--' +y);
+
+              if(this.y >= y){
+                return;
+              }
+              v.style.top =  v.style.top ? parseFloat(v.style.top)-1+'px' : 0+'px';   
               console.log('w');
               break;
             }
             case 'a' : {
-              const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
+             
               const v = document.getElementById(this.selectedBoxId);
-              v.style.left = x-1+'px';
+              const {x,y} = v.getBoundingClientRect();
+              if(this.x >= x){
+                return;
+              }
+              v.style.left = v.style.left ?  parseFloat(v.style.left)-1+'px' : 0+'px';
               console.log('a');
               break;
             }
             case 's' : {
-              const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
+              // const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
               const v = document.getElementById(this.selectedBoxId);
-              v.style.top = y+1+'px';
+              const {x,y,width,height} = v.getBoundingClientRect();
+              if((this.y+this.height) <= (y+height)){
+                return;
+              }
+              v.style.top = v.style.top ? parseFloat(v.style.top)+1+'px' : 0+'px';
               console.log('s');
               break;
             }
             case 'd' : {
-              const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
+              // const {x, y} = this.renderer.selectRootElement(document.getElementById(this.selectedBoxId).getBoundingClientRect());
               const v = document.getElementById(this.selectedBoxId);
-              v.style.left = x+1+'px';
+              const {x,y,width,height} = v.getBoundingClientRect();
+
+              if((this.x+this.width) <= (x+width)){
+                return;
+              }
+              v.style.left = v.style.left ? parseFloat(v.style.left)+1+'px' : 0+'px';
               console.log('d');
+              break;
+            }
+            case 'Delete' : {
+              console.log('Delete is pressed');
+              if(this.testBox.nativeElement.childNodes.length <= 0){
+                return;
+              }
+              const elementToBeDeleted = this.selectedBoxId;
+
+              if(this.renderer.nextSibling(
+                this.renderer.selectRootElement(document.getElementById(this.selectedBoxId))
+                )){
+                  this.selectedBoxId = this.renderer.nextSibling(this.renderer.selectRootElement(document.getElementById(this.selectedBoxId))).id;
+                  this.boxClicked(this.selectedBoxId);
+                }else{
+                  this.selectedBoxId = this.testBox.nativeElement.childNodes[0].id;
+                  this.boxClicked(this.selectedBoxId);
+                }
+
+              this.renderer.removeChild(
+                this.testBox.nativeElement,
+                this.renderer.selectRootElement(document.getElementById(elementToBeDeleted))
+              );
+              
               break;
             }
             default : {console.log('no');break;}
           }
+        }else{
+          alert('Please select a box');
         }
       });
  
